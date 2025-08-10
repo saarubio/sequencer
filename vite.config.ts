a79d35bfa039e7af7b26d0b15f19d1d78d6b0d3d
Steve <saarubio@gmail.com>
2025-08-11 00:15:43 +0300

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(({  }) => {
     return {
+      base: '/sequencer/',
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
